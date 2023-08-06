48a1d2556fb6857e6735c3356692c7baf26ec8ed
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-06 15:16:06 +0200

    add cors

--- app-js-fullStack/backend/server.js
+++ app-js-fullStack/backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express'); // import express
+const cors = require('cors'); // import cors
 const connectDB = require('../config/db');
 const dotenv = require('dotenv') // import dotenv
 
@@ -17,7 +18,10 @@ const app = express(); // create express app
 /** Middleware : permet de traiter les données avant de les envoyer au client (Req) 
 ou 
 avant de les envoyer à la base de données (Res) (ex : vérifier si l'utilisateur est connecté avant de lui envoyer les données)  ; **/
-app.use(express.json()); // use express middleware to parse json bodies
+app.use(cors({
+    origin: 'http://localhost:3000',
+  })); // use cors middleware to allow cross-origin requests
+  app.use(express.json()); // use express middleware to parse json bodies
 app.use(express.urlencoded({ extended: false })); // use express middleware to parse urlencoded bodies
 
 app.use("/post", require("./routes/post.routes")); // use post routes for requests to /post
